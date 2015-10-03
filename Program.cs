085206e01de456cfa5533bcd248f2f40e33c6a79
Andrey Samoykin <samoykin@gmail.com>
2015-10-03 23:03:17 +0600

    Версия 1.0
    Проверяет поменялся ли номер версии у Phonebook в файле prop.xml и при изменении обновляет

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhonebookUpdater
+{
+    class Program
+    {
+        private static String pathToXml = @"Updater\UpdProp.xml";
+        private static String pathToTemp = Environment.CurrentDirectory + @"\Temp\";
+        private static String pathToUpd = "";
+        private static String excl1 = "Updater";
+        private static String excl2 = "Temp";
+
+        private static List<String> ch = new List<string>();
+
+        private static XMLcode xml = new XMLcode();
+
+        static void Main(string[] args)
+        {
+            //удаление процесса
+            string target_name = "Phonebook";
+            System.Diagnostics.Process[] local_procs = System.Diagnostics.Process.GetProcesses();
+            try
+            {
+                System.Diagnostics.Process target_proc = local_procs.First(p => p.ProcessName == target_name);
+                target_proc.Kill();
+            }
+            catch { }
+
+            if (!File.Exists(pathToXml))
+            {
+                xml.CreateXml();
+                xml.CreateNodesXml();
+                xml.WriteXml();
+            }
+            pathToUpd = xml.ReadXml();
+
+
+                if (pathToUpd != "")
+                {
+                    ch = xml.ReadXmlForUpdate(pathToUpd);
+                    UpdSoft();
+                }
+                else
+                {
+                    Console.WriteLine("Неверный путь к файлу конфигурации " + pathToUpd);
+                    
+                }
+
+
+            
+        }
+
+        static void UpdSoft()
+        {
+            pathToUpd = ch[1];
+
+            if (Directory.Exists(pathToTemp))
+            {
+                delFromDir(pathToTemp);
+            }
+
+            Directory.CreateDirectory(pathToTemp);
+
+            CopyDir(ch[1], pathToTemp);
+
+            string[] fullfilesPath = Directory.GetFiles(Environment.CurrentDirectory);
+            string[] fullDirPath = Directory.GetDirectories(Environment.CurrentDirectory);
+
+            string temp;
+
+            foreach (var s in fullfilesPath)
+            {
+                File.Delete(s);
+            }
+
+            foreach (var s in fullDirPath)
+            {
+                temp = Path.GetFileNameWithoutExtension(s);
+                if (temp != excl1 && temp != excl2)
+                {
+                    DirectoryInfo dir = new DirectoryInfo(s);
+                    dir.Delete(true);
+                }
+            }
+
+            CopyDir(pathToTemp, Environment.CurrentDirectory);
+
+            delFromDir(pathToTemp);
+            Process.Start(@"Phonebook.exe");
+        }
+
+        static void CopyDir(string FromDir, string ToDir)
+        {
+            Directory.CreateDirectory(ToDir);
+            foreach (string s1 in Directory.GetFiles(FromDir))
+            {
+                string s2 = ToDir + "\\" + Path.GetFileName(s1);
+                File.Copy(s1, s2);
+            }
+            foreach (string s in Directory.GetDirectories(FromDir))
+            {
+                CopyDir(s, ToDir + "\\" + Path.GetFileName(s));
+            }
+        }
+
+        static void delFromDir(String dirPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(dirPath);
+
+            foreach (FileInfo file in dir.GetFiles()) file.Delete();
+            foreach (DirectoryInfo subDirectory in dir.GetDirectories()) subDirectory.Delete(true);
+
+        }
+         
+    }
+}
---
+++ XMLcode.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PhonebookUpdater
+{
+    class XMLcode
+    {
+        private string pathToXml = @"Updater\UpdProp.xml";
+        private string pathForUpd = @"d:\Temp\prop.xml";
+        private string exQ = "0";
+
+        public void CreateXml()
+        {
+            XmlTextWriter textWritter = new XmlTextWriter(pathToXml, Encoding.UTF8);
+            textWritter.WriteStartDocument();
+            textWritter.WriteStartElement("head");
+            textWritter.WriteEndElement();
+            textWritter.Close();
+        }
+
+        public void CreateNodesXml()
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(pathToXml);
+
+            XmlNode upd = document.CreateElement("Update");
+            document.DocumentElement.AppendChild(upd);
+
+            XmlNode t1 = document.CreateElement("path");
+            t1.InnerText = "1";
+            upd.AppendChild(t1);
+
+            XmlNode ver = document.CreateElement("versionUpd");
+            ver.InnerText = "1";
+            upd.AppendChild(ver);
+
+            XmlNode t2 = document.CreateElement("exQ");
+            t2.InnerText = "1";
+            upd.AppendChild(t2);
+
+            XmlNode t3 = document.CreateElement("ex1");
+            t3.InnerText = "1";
+            upd.AppendChild(t3);
+
+            XmlNode t4 = document.CreateElement("ex2");
+            t4.InnerText = "1";
+            upd.AppendChild(t4);
+
+            XmlNode t5 = document.CreateElement("ex3");
+            t5.InnerText = "1";
+            upd.AppendChild(t5);
+
+            XmlNode t6 = document.CreateElement("ex4");
+            t6.InnerText = "1";
+            upd.AppendChild(t6);
+
+            XmlNode t7 = document.CreateElement("ex5");
+            t7.InnerText = "1";
+            upd.AppendChild(t7);
+
+
+
+            document.Save(pathToXml);
+
+        }
+
+        public void WriteXml()
+        {
+
+            XmlDocument document = new XmlDocument();
+            document.Load(pathToXml);
+
+            XmlElement xRoot = document.DocumentElement;
+            foreach (XmlElement xnode in xRoot)
+            {
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "path")
+                        childnode.InnerText = pathForUpd;
+                    if (childnode.Name == "versionUpd")
+                        childnode.InnerText = Assembly.GetCallingAssembly().GetName().Version.ToString();
+                }
+            }
+            document.Save(pathToXml);
+        }
+
+        public String ReadXml()
+        {
+            String ch = "";
+
+            XmlDocument document = new XmlDocument();
+            document.Load(pathToXml);
+
+            XmlElement xRoot = document.DocumentElement;
+            foreach (XmlElement xnode in xRoot)
+            {
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "path")
+                        ch = childnode.InnerText;
+                }
+            }
+
+            return ch;
+        }
+
+        public List<String> ReadXmlForUpdate(string path)
+        {
+            //String[] ch = new String[27];
+            List<String> ch = new List<string>();
+
+            XmlDocument document = new XmlDocument();
+            document.Load(path);
+
+            XmlElement xRoot = document.DocumentElement;
+            foreach (XmlElement xnode in xRoot)
+            {
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    //параметры PhonebookUpdater
+                    if (childnode.Name == "version")
+                        ch.Add(childnode.InnerText);
+                    if (childnode.Name == "path")
+                        ch.Add(childnode.InnerText);
+                    if (childnode.Name == "versionUpd")
+                        ch.Add(childnode.InnerText);
+                    if (childnode.Name == "pathUpd")
+                        ch.Add(childnode.InnerText);
+                }
+            }
+            return ch;
+        }
+
+
+    }
+}
